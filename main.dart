a97909a8da7e97737b1935ebfa29d38a1a6f7152
Mujiono90 <jiomuji90@gmail.com>
2026-02-10 13:00:10 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,7 +1,12 @@
 void main(){
-for (var i = 1; i <= 10; i++) {
-if (i == 5) break;
-if (i % 2 == 0) continue;
-print("Angka ganjil: $i");
+var skor = 85;
+if (skor >= 90) {
+print("Grade: A");
+} else if (skor >= 80) {
+print("Grade: B");
+} else if (skor >= 70) {
+print("Grade: C");
+} else {
+print("Grade: D");
 }
 }
